db22c1855b68af565233a2387a5c1a8077280872
MohdAdnanKhan001 <adnankhanmak1786@gmail.com>
2025-03-19 14:03:24 +0530

    debug=false

--- rag/main.py
+++ rag/main.py
@@ -74,4 +74,4 @@ def rate_limit_handler(request: Request, exc: RateLimitExceeded):
 # Ensure the app runs on Render's provided port
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))  # Render provides a PORT variable
-    uvicorn.run(app, host="0.0.0.0", port=port)
+    uvicorn.run(app, host='0.0.0.0', port=port, debug=False)
